e01096007730278b7d9bd71494b6779af71e9b56
Enyi Shang <enyi.shang16@imperial.ac.uk>
2021-02-27 01:56:30 +0000

    Add storybook in readme and import the highcharts module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScoreChartComponent } from '@components/score-chart/score-chart.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
     declarations: [AppComponent, ScoreChartComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, HighchartsChartModule],
     providers: [],
     bootstrap: [AppComponent],
 })
