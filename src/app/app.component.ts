6c842f9a80254ec88ce8b40df019ba72c0d64915
Anson Miu <me@ansonmiu.dev>
2021-03-04 12:32:34 +0000

    Use ArticleComparisonView in app component to experiment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = "Cockatoos";
+
+    document = {
+        text: 'Hello world',
+        phrases: [
+          {
+            startIndex: 0,
+            endIndex: 10,
+          }
+        ]
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ScoreChartComponent } from '@components/score-chart/score-chart.component';
-import { HighchartsChartModule } from 'highcharts-angular';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ScoreChartComponent } from "@components/score-chart/score-chart.component";
+import { HighchartsChartModule } from "highcharts-angular";
+import { ArticleComparisonViewComponent } from "@components/article-comparison/article-comparison-view.component";
 
 @NgModule({
-    declarations: [AppComponent, ScoreChartComponent],
+    declarations: [AppComponent, ScoreChartComponent, ArticleComparisonViewComponent],
     imports: [BrowserModule, AppRoutingModule, HighchartsChartModule],
     providers: [],
     bootstrap: [AppComponent],
