c4861a5f847e1cdb6d802c810ea7280a13b1628d
Anson Miu <me@ansonmiu.dev>
2021-03-06 21:40:24 +0000

    Change Observable -> BehaviourSubject

--- src/app/services/text-to-speech.service.ts
+++ src/app/services/text-to-speech.service.ts
@@ -1,20 +1,19 @@
 import { Inject, Injectable } from "@angular/core";
 import { SPEECH_SYNTHESIS } from "@ng-web-apis/common";
-import { Observable, of } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
     providedIn: "root",
 })
 export class TextToSpeechService {
-
     // Exposes the speaking state, i.e. true iff speaking.
-    speaking$: Observable<boolean>;
+    speaking$: BehaviorSubject<boolean>;
 
     constructor(
         @Inject(SPEECH_SYNTHESIS)
         private readonly speechSynthesis: SpeechSynthesis
     ) {
-        this.speaking$ = of(false);
+        this.speaking$ = new BehaviorSubject(false);
     }
 
     /**
@@ -28,17 +27,13 @@ export class TextToSpeechService {
      * Perform text-to-speech on the @param text.
      */
     speak(text: string): void {
-        this.speaking$ = new Observable((subscriber) => {
-            const utterance = new SpeechSynthesisUtterance(text);
-            utterance.addEventListener("start", () => {
-                subscriber.next(true);
-            });
-            utterance.addEventListener("end", () => {
-                subscriber.next(false);
-            });
-
-            this.speechSynthesis.speak(utterance);
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.addEventListener("start", () => {
+            this.speaking$.next(true);
         });
+        utterance.addEventListener("end", () => {
+            this.speaking$.next(false);
+        });
+        this.speechSynthesis.speak(utterance);
     }
-
 }
