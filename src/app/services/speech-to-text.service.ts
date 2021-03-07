a5d9f708fc1cbf518bec340a4b30b900a883cda1
Anson Miu <me@ansonmiu.dev>
2021-03-07 14:23:56 +0000

    Fix SpeechRecognition integration

--- src/app/services/speech-to-text.service.ts
+++ src/app/services/speech-to-text.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@angular/core";
+import { Inject, Injectable, Type } from "@angular/core";
 import { SPEECH_RECOGNITION } from "@ng-web-apis/common";
 import { BehaviorSubject } from "rxjs";
 
@@ -8,13 +8,28 @@ import { BehaviorSubject } from "rxjs";
 export class SpeechToTextService {
     transcript$: BehaviorSubject<string>;
     recording$: BehaviorSubject<boolean>;
+    speechRecognition: SpeechRecognition;
 
     constructor(
         @Inject(SPEECH_RECOGNITION)
-        private readonly speechRecognition: SpeechRecognition
+        private readonly SpeechRecognition: Type<SpeechRecognition>
     ) {
+        this.speechRecognition = new this.SpeechRecognition();
         this.recording$ = new BehaviorSubject(false);
         this.transcript$ = new BehaviorSubject("");
+
+        this.initialiseSpeechRecognition();
+    }
+
+    private initialiseSpeechRecognition(): void {
+        this.speechRecognition.continuous = true;
+        this.speechRecognition.interimResults = true;
+        this.speechRecognition.addEventListener("result", (ev) => {
+            const transcript = Array.from(ev.results)
+                .map((result) => result[0].transcript)
+                .join(" ");
+            this.transcript$.next(transcript);
+        });
     }
 
     get available(): boolean {
@@ -23,12 +38,6 @@ export class SpeechToTextService {
 
     start(): void {
         this.recording$.next(true);
-        this.speechRecognition.onresult = (ev) => {
-            const transcript = Array.from(ev.results)
-                .map((result) => result[0].transcript)
-                .join(" ");
-            this.transcript$.next(transcript);
-        };
         this.speechRecognition.start();
     }
 
