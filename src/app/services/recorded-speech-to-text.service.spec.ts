d14518b614e0d9e392ee2757c71086d903cc6033
Anson Miu <me@ansonmiu.dev>
2021-03-07 14:24:17 +0000

    Implement wrapper service to manage audio recording and speech recognition

---
+++ src/app/services/recorded-speech-to-text.service.spec.ts
@@ -0,0 +1,16 @@
+// import { TestBed } from "@angular/core/testing";
+
+// import { RecordedSpeechToTextService } from "./recorded-speech-to-text.service";
+
+// describe("RecordedSpeechToTextService", () => {
+//     let service: RecordedSpeechToTextService;
+
+//     beforeEach(() => {
+//         TestBed.configureTestingModule({});
+//         service = TestBed.inject(RecordedSpeechToTextService);
+//     });
+
+//     it("should be created", () => {
+//         expect(service).toBeTruthy();
+//     });
+// });
---
+++ src/app/services/recorded-speech-to-text.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, merge, Observable, of } from "rxjs";
+import { reduce } from "rxjs/operators";
+import { AudioRecorderService } from "./audio-recorder.service";
+import { SpeechToTextService } from "./speech-to-text.service";
+
+@Injectable({
+    providedIn: "root",
+})
+export class RecordedSpeechToTextService {
+    constructor(
+        private readonly audioRecorder: AudioRecorderService,
+        private readonly speechToText: SpeechToTextService
+    ) {}
+
+    get transcript$(): BehaviorSubject<string> {
+        return this.speechToText.transcript$;
+    }
+
+    get available$(): Observable<boolean> {
+        return merge(this.audioRecorder.available$, of(this.speechToText.available)).pipe(
+            reduce((acc, val) => acc && val, true)
+        );
+    }
+
+    start(): void {
+        this.audioRecorder.start();
+        this.speechToText.start();
+    }
+
+    get recording$(): BehaviorSubject<boolean> {
+        return this.speechToText.recording$;
+    }
+
+    stop(): void {
+        this.audioRecorder.stop();
+        this.speechToText.stop();
+    }
+}
