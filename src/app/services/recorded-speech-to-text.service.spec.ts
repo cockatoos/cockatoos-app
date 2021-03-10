01f9a6c8ed79488f5d9a0a85d069c1eccaeb8980
Anson Miu <me@ansonmiu.dev>
2021-03-10 21:14:55 +0000

    Fix test and linting

--- src/app/services/recorded-speech-to-text.service.spec.ts
+++ src/app/services/recorded-speech-to-text.service.spec.ts
@@ -1,16 +1,42 @@
-// import { TestBed } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
+import { AudioRecorderService } from "./audio-recorder.service";
 
-// import { RecordedSpeechToTextService } from "./recorded-speech-to-text.service";
+import { RecordedSpeechToTextService } from "./recorded-speech-to-text.service";
+import { SpeechToTextService } from "./speech-to-text.service";
 
-// describe("RecordedSpeechToTextService", () => {
-//     let service: RecordedSpeechToTextService;
+describe("RecordedSpeechToTextService", () => {
+    let service: RecordedSpeechToTextService;
+    let audioRecorderServiceSpy: jasmine.SpyObj<AudioRecorderService>;
+    let speechToTextServiceSpy: jasmine.SpyObj<SpeechToTextService>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(RecordedSpeechToTextService);
-//     });
+    beforeEach(() => {
+        const mockMethods = ["start", "stop", "reset"];
 
-//     it("should be created", () => {
-//         expect(service).toBeTruthy();
-//     });
-// });
+        const mockAudioRecorderService = jasmine.createSpyObj("AudioRecorderService", mockMethods);
+        const mockSpeechToTextService = jasmine.createSpyObj("SpeechToTextService", mockMethods);
+
+        TestBed.configureTestingModule({
+            // Provide both the service-to-test and its (spy) dependencies.
+            providers: [
+                RecordedSpeechToTextService,
+                {
+                    provide: AudioRecorderService,
+                    useValue: mockAudioRecorderService,
+                },
+                {
+                    provide: SpeechToTextService,
+                    useValue: mockSpeechToTextService,
+                },
+            ],
+        });
+
+        // Inject both the service-to-test and its (spy) dependencies.
+        service = TestBed.inject(RecordedSpeechToTextService);
+        audioRecorderServiceSpy = TestBed.inject(AudioRecorderService) as jasmine.SpyObj<AudioRecorderService>;
+        speechToTextServiceSpy = TestBed.inject(SpeechToTextService) as jasmine.SpyObj<SpeechToTextService>;
+    });
+
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
+});
