2cf64707cb80f3b2ed9fcd3e41cbc50fa4c6c3fd
Enyi Shang <enyi.shang16@imperial.ac.uk>
2021-03-14 23:13:33 +0000

    Fix linting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
             {
                 path: '', redirectTo: '/app/scores', pathMatch: 'full'
             }
-        ],},
+        ], },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
