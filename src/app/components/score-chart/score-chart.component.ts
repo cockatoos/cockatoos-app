b11156a641ac5ad508ba09aa971550a699226f75
Enyi Shang <enyi.shang16@imperial.ac.uk>
2021-03-30 23:17:48 +0100

    lint fix??

--- src/app/components/score-chart/score-chart.component.ts
+++ src/app/components/score-chart/score-chart.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import * as Highcharts from 'highcharts';
-import more from 'highcharts/highcharts-more';
-import { parseISO } from 'date-fns';
-import { mean } from 'lodash';
-import HighchartsBoost from 'highcharts/modules/boost';
-import HCSoldGauge from 'highcharts/modules/solid-gauge';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
+import * as Highcharts from "highcharts";
+import more from "highcharts/highcharts-more";
+import { parseISO } from "date-fns";
+import { mean } from "lodash";
+import HighchartsBoost from "highcharts/modules/boost";
+import HCSoldGauge from "highcharts/modules/solid-gauge";
 
 more(Highcharts);
 HighchartsBoost(Highcharts);
@@ -51,72 +51,76 @@ export class ScoreChartComponent implements OnChanges {
         return todayScore[0].score * 100;
     }
 
-  public gaugeOptions: Highcharts.Options = {
-    title: {
-      text: undefined
-    },
-    yAxis: {
-      stops: [
-          [0.2, '#DF5353'], // red
-          [0.5, '#DDDF0D'], // yellow
-          [0.8, '#55BF3B'] // green
-      ],
-      lineWidth: 0,
-      tickWidth: 0,
-      minorTickInterval: null,
-      tickAmount: 2,
-      min: 0,
-      max: 100,
-      title: {
-          text: undefined
-      }
-    },
-    plotOptions: {
-      solidgauge: {
-          dataLabels: {
-              borderWidth: 0,
-              useHTML: true
-          },
-          rounded: true,
-          linecap: 'round',
-      }
-    },
-    pane: {
-      center: ['50%', '50%'],
-      size: '160%',
-      startAngle: -90,
-      endAngle: 270,
-      background: [{ // Track for Exercise
-          outerRadius: '62%',
-          innerRadius: '38%',
-          backgroundColor: '#fff6f6',
-          borderWidth: 0,
-      }],
-    },
-    series: [{
-      type: 'solidgauge',
-      radius: '62%',
-      innerRadius: '38%',
-      data: [20],
-      dataLabels: {
-        format:
-            '<div style="text-align:center;">' +
-              '<span style="font-size:40px;font-weight: 700; color: #304d86">{y}</span><br/>' +
-            '</div>',
-          x: 0,
-          y: -30
-      },
-    }],
-    tooltip: {
-      enabled: false
-    },
-    chart: {
-      reflow: true,
-      height: 220,
-      width: 220
-    }
-
-  };
+    public gaugeOptions: Highcharts.Options = {
+        title: {
+            text: undefined,
+        },
+        yAxis: {
+            stops: [
+                [0.2, "#DF5353"], // red
+                [0.5, "#DDDF0D"], // yellow
+                [0.8, "#55BF3B"], // green
+            ],
+            lineWidth: 0,
+            tickWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 2,
+            min: 0,
+            max: 100,
+            title: {
+                text: undefined,
+            },
+        },
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    borderWidth: 0,
+                    useHTML: true,
+                },
+                rounded: true,
+                linecap: "round",
+            },
+        },
+        pane: {
+            center: ["50%", "50%"],
+            size: "160%",
+            startAngle: -90,
+            endAngle: 270,
+            background: [
+                {
+                    // Track for Exercise
+                    outerRadius: "62%",
+                    innerRadius: "38%",
+                    backgroundColor: "#fff6f6",
+                    borderWidth: 0,
+                },
+            ],
+        },
+        series: [
+            {
+                type: "solidgauge",
+                radius: "62%",
+                innerRadius: "38%",
+                data: [20],
+                dataLabels: {
+                    format:
+                        '<div style="text-align:center;">' +
+                        '<span style="font-size:40px;font-weight: 700; color: #304d86">{y}</span><br/>' +
+                        "</div>",
+                    x: 0,
+                    y: -30,
+                },
+            },
+        ],
+        tooltip: {
+            enabled: false,
+        },
+        chart: {
+            reflow: true,
+            height: 220,
+            width: 220,
+        },
+    };
 
     public chartOptions: Highcharts.Options = {
         title: {
@@ -127,7 +131,6 @@ export class ScoreChartComponent implements OnChanges {
             title: {
                 text: `Score`,
             },
-
         },
 
         xAxis: {
@@ -156,26 +159,21 @@ export class ScoreChartComponent implements OnChanges {
         },
         series: [],
         chart: {
-            type: 'column',
+            type: "column",
             reflow: true,
             width: 350,
             height: 300,
         },
         plotOptions: {
             series: {
-              marker: {
-                enabled: true,
-                symbol: 'circle',
-                radius: 8
-            }
-            }
-        }
+                marker: {
+                    enabled: true,
+                    symbol: "circle",
+                    radius: 8,
+                },
+            },
+        },
     };
-
-    ngOnInit(): void {
-        this.height = 20;
-        this.width = 20;
-    }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes && changes.historicalData) {
@@ -184,7 +182,10 @@ export class ScoreChartComponent implements OnChanges {
                     name: `${this.label} Scores`,
                     type: "line",
                     color: "#f24405",
-                    data: changes.historicalData.currentValue.map(({ date, score }: Score) => [date.getTime(), score * 100]),
+                    data: changes.historicalData.currentValue.map(({ date, score }: Score) => [
+                        date.getTime(),
+                        score * 100,
+                    ]),
                 },
             ];
         }
